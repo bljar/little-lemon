710a89a24b658ecba3e2f4c7c1c3823270f7a658
Belajar <bljkmp@gmail.com>
2023-04-13 05:49:54 +0700

    Add little lemon logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,13 @@
+import logo from '../images/Logo.svg';
+
 function Nav() {
   return (
     <nav>
-      
+      <ul>
+        <li>
+          <img src={logo} alt='Logo' />
+        </li>
+      </ul>
     </nav>
   )
 }
