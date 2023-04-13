9001b63ed61fbb4c7463feedb27b76e290acaa16
Belajar <bljkmp@gmail.com>
2023-04-13 08:50:18 +0700

    Add meta tags and OGP data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import logo from '../images/Logo.svg';
+
 function Header() {
+  const description = 'Mediterranean restaurant, focused on traditional recipes served with a modern twist';
   return (
     <header>
-      
+      <meta name='description' content={description}/>
+      <meta name='og:title' content="Little Lemon Restaurant's"/>
+      <meta name='og:description' content={description}/>
+      <meta name='og:image' content={logo}/>
     </header> 
   )
 }
