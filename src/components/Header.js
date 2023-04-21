8a3d6d20e2e20f3c8d4c0311734d92ef1c49b74a
Belajar <bljkmp@gmail.com>
2023-04-22 06:55:35 +0700

    move OGP metatags to correct place

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,6 @@
-import logo from '../images/Logo.svg';
-
 function Header() {
-  const description = 'Mediterranean restaurant, focused on traditional recipes served with a modern twist';
   return (
     <header>
-      <meta name='description' content={description}/>
-      <meta name='og:title' content="Little Lemon Restaurant's"/>
-      <meta name='og:description' content={description}/>
-      <meta name='og:image' content={logo}/>
     </header> 
   )
 }
