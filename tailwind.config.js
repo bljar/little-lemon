3b1404d9314cdd4aec1f03b192d78920c24e45aa
Belajar <bljkmp@gmail.com>
2023-05-15 19:13:55 +0700

    Migrate to vite+tailwind framework

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+export default {
+  content: [
+    "./src/index.html",
+    "./src/pages/**/*.{tsx,js}",
+    "./src/components/**/*.{tsx,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          1: "#495e57",
+          2: "#f4ce14",
+        },
+        secondary: {
+          1: "#ee9972",
+          2: "#fbdabb",
+        },
+        highlight: {
+          1: "#edefee",
+          2: "#333333",
+        },
+      },
+      fontFamily: {
+        sans: ["Karla", ...defaultTheme.fontFamily.sans],
+        serif: ["Markazi Text", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+};
