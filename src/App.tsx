d866cda3c85d4c7da50eedbb443caa9899d31b1d
Belajar <bljkmp@gmail.com>
2023-05-17 08:41:37 +0700

    create reservation page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import Reservation from "./pages/Reservation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/reservation" element={<Reservation />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/Reservation.tsx
@@ -0,0 +1,5 @@
+function Reservation() {
+  return <main>Hello</main>;
+}
+
+export default Reservation;
