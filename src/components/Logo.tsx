8ed8fbbdc1085fa8a2900db0d57779fe422c9a67
Belajar <bljkmp@gmail.com>
2023-05-20 16:31:18 +0700

    add chevron icon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,9 @@
 interface logoProps {
   className: string;
 }
+interface iconProp {
+  className: string;
+}
 
 export function Logo(props: logoProps) {
   return (
@@ -150,3 +153,37 @@ export function FooterLogo(props: logoProps) {
     </svg>
   );
 }
+
+export const ChevronRight = (props: iconProp) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className={props.className}
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export const ChevronLeft = (props: iconProp) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className={props.className}
+    >
+      <path
+        fillRule="evenodd"
+        d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
