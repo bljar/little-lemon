4f48283f36e4c6dbb1fda9d21570808751aedf1c
Belajar <bljkmp@gmail.com>
2023-05-24 08:45:40 +0700

    finalize reservation form

--- src/components/ResForm.tsx
+++ src/components/ResForm.tsx
@@ -1,8 +1,9 @@
 import DatePicker from "./DatePicker";
 import MyListBox from "./MyListBox";
 import { useEffect, useState } from "react";
-import { RadioGroup } from "@headlessui/react";
-import { RadioChecked, RadioUnchecked } from "./Icons";
+import RestaurantImg from "../assets/Restaurant.jpg";
+import RestaurantChef from "../assets/RestaurantChef.jpg";
+import RestaurantFood from "../assets/RestaurantFood.jpg";
 
 function ResForm() {
   const [resDate, setResDate] = useState(null);
@@ -91,92 +92,120 @@ function ResForm() {
   ];
 
   return (
-    <section id="reservation" className="bg-primary-1">
-      <h1 className="mx-auto max-w-4xl p-5 pb-10 text-center text-primary-2 sm:text-left">
-        Reserve a table
-      </h1>
-      <form className="mx-auto grid max-w-4xl gap-8 px-5 font-sans sm:grid-cols-2">
-        <label
-          htmlFor="indoor-seating"
-          className="relative mx-auto flex h-min w-full max-w-sm items-center gap-x-4 sm:ml-0"
-        >
-          <input
-            className="border-2 border-highlight-1 bg-primary-1 checked:border-2 checked:border-highlight-1 checked:bg-primary-1 hover:checked:bg-primary-1 focus:text-primary-1 focus:ring-0 focus:checked:ring-0"
-            type="radio"
-            id="indoor-seating"
-            name="seating"
-            value="indoor"
+    <>
+      <section id="reservation" className="bg-primary-1 pb-10">
+        <h1 className="mx-auto max-w-4xl p-5 pb-5 text-center text-primary-2 sm:text-left">
+          Reservation
+        </h1>
+        <form className="mx-auto grid max-w-4xl gap-8 px-5 font-sans sm:grid-cols-2">
+          <label
+            htmlFor="indoor-seating"
+            className="relative mx-auto flex h-min w-full max-w-sm items-center gap-x-4 sm:ml-0"
+          >
+            <input
+              className="border-2 border-highlight-1 bg-primary-1 checked:border-2 checked:border-highlight-1 checked:bg-primary-1 hover:checked:bg-primary-1 focus:text-primary-1 focus:ring-0 focus:checked:ring-0"
+              type="radio"
+              id="indoor-seating"
+              name="seating"
+              value="indoor"
+            />
+            <span className="text-xl font-bold text-highlight-1">
+              Indoor seating
+            </span>
+          </label>
+          <label
+            htmlFor="outdoor-seating"
+            className="relative mx-auto flex h-full w-full max-w-sm items-center gap-x-4 sm:mr-0"
+          >
+            <input
+              className="border-2 border-highlight-1 bg-primary-1 checked:border-2 checked:border-highlight-1 checked:bg-primary-1 hover:checked:bg-primary-1 focus:text-primary-1 focus:ring-0 focus:checked:ring-0"
+              type="radio"
+              id="outdoor-seating"
+              name="seating"
+              value="outdoor"
+            />
+            <span className="text-xl font-bold text-highlight-1">
+              Outdoor seating
+            </span>
+          </label>
+          <label htmlFor="date">
+            <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:ml-0">
+              Date
+            </span>
+            <DatePicker
+              id="date"
+              className="relative mx-auto h-full max-w-sm sm:ml-0"
+              selectedDate={resDate}
+              setSelectedDate={setResDate}
+            />
+          </label>
+          <label htmlFor="reservation-guests">
+            <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:mr-0">
+              Number of Diners
+            </span>
+            <MyListBox
+              id="guests"
+              className="relative mx-auto h-full max-w-sm sm:mr-0"
+              selected={resGuest}
+              setSelected={setResGuest}
+              placeholder="No. of Diners"
+              icon="person"
+              lists={numDiners}
+            />
+          </label>
+          <label htmlFor="reservation-occasion">
+            <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:ml-0">
+              Occasion
+            </span>
+            <MyListBox
+              id="occasion"
+              className="relative mx-auto h-full max-w-sm sm:ml-0"
+              selected={resOccasion}
+              setSelected={setResOccasion}
+              placeholder="Occasion"
+              icon="wine-glasses"
+              lists={occasions}
+            />
+          </label>
+          <label htmlFor="reservation-time">
+            <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:mr-0">
+              Time
+            </span>
+            <MyListBox
+              id="time"
+              className="relative mx-auto h-full max-w-sm sm:mr-0"
+              selected={resTime}
+              setSelected={setResTime}
+              placeholder="Select time"
+              icon="alarm"
+              lists={availableTime}
+            />
+          </label>
+        </form>
+      </section>
+      <section className="my-5">
+        <figure className="mx-auto flex max-w-sm justify-between gap-x-5 overflow-x-clip p-5 hover:overflow-x-auto sm:max-w-4xl">
+          <img
+            src={RestaurantImg}
+            alt="Restaurant Image"
+            className="aspect-[6/5] w-60 rounded-xl object-cover drop-shadow-xl"
           />
-          <span className="text-xl font-bold text-highlight-1">
-            Indoor seating
-          </span>
-        </label>
-        <label
-          htmlFor="outdoor-seating"
-          className="relative mx-auto flex h-full w-full max-w-sm items-center gap-x-4 sm:mr-0"
-        >
-          <input
-            className="border-2 border-highlight-1 bg-primary-1 checked:border-2 checked:border-highlight-1 checked:bg-primary-1 hover:checked:bg-primary-1 focus:text-primary-1 focus:ring-0 focus:checked:ring-0"
-            type="radio"
-            id="outdoor-seating"
-            name="seating"
-            value="outdoor"
+          <img
+            src={RestaurantChef}
+            alt="Restaurant Chef"
+            className="aspect-[6/5] w-60 rounded-xl object-cover drop-shadow-xl"
           />
-          <span className="text-xl font-bold text-highlight-1">
-            Outdoor seating
-          </span>
-        </label>
-        <label htmlFor="date">
-          <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:ml-0">
-            Date
-          </span>
-          <DatePicker
-            className="relative mx-auto h-full max-w-sm sm:ml-0"
-            selectedDate={resDate}
-            setSelectedDate={setResDate}
+          <img
+            src={RestaurantFood}
+            alt="Restaurant Food"
+            className="aspect-[6/5] w-60 rounded-xl object-cover drop-shadow-xl"
           />
-        </label>
-        <label htmlFor="reservation-guests">
-          <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:mr-0">
-            Number of Diners
-          </span>
-          <MyListBox
-            className="relative mx-auto h-full max-w-sm sm:mr-0"
-            selected={resGuest}
-            setSelected={setResGuest}
-            placeholder="No. of Diners"
-            icon="person"
-            lists={numDiners}
-          />
-        </label>
-        <label htmlFor="reservation-occasion">
-          <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:ml-0">
-            Occasion
-          </span>
-          <MyListBox
-            className="relative mx-auto h-full max-w-sm sm:ml-0"
-            selected={resOccasion}
-            setSelected={setResOccasion}
-            placeholder="Occasion"
-            icon="wine-glasses"
-            lists={occasions}
-          />
-        </label>
-        <label htmlFor="reservation-time">
-          <span className="mx-auto mb-1 block max-w-sm text-highlight-1 sm:mr-0">
-            Time
-          </span>
-          <MyListBox
-            className="relative mx-auto h-full max-w-sm sm:mr-0"
-            selected={resTime}
-            setSelected={setResTime}
-            placeholder="Select time"
-            icon="alarm"
-            lists={availableTime}
-          />
-        </label>
-      </form>
-    </section>
+        </figure>
+        <button className=" mx-auto mb-20 mt-10 block rounded-xl bg-primary-2 px-20 py-2 text-xl font-bold drop-shadow hover:drop-shadow-xl">
+          <a href="">Reserve a table</a>
+        </button>
+      </section>
+    </>
   );
 }
 
